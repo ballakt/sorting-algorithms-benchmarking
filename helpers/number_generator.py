025e108ac96e785281ff787d17583283197f71d2
Tomáš Ballák <ballak.tomas@protonmail.com>
2021-01-31 19:29:59 +0100

    sorted integers

--- helpers/number_generator.py
+++ helpers/number_generator.py
@@ -4,8 +4,10 @@ import numpy as np
 NUM=5000
 FILENAME_RANDOM_INTEGERS=f'random_integers_{NUM}'
 FILENAME_RANDOM_DOUBLES=f'random_doubles_{NUM}'
+FILENAME_SORTED_INTEGERS=f'sorted_integers_{NUM}'
 FILENAME_RANDOM_INTEGERS_CPP=f'{FILENAME_RANDOM_INTEGERS}.cpp'
 FILENAME_RANDOM_DOUBLES_CPP=f'{FILENAME_RANDOM_DOUBLES}.cpp'
+FILENAME_SORTED_INTEGERS_CPP=f'{FILENAME_SORTED_INTEGERS}.cpp'
 END=".txt"
 
 def gen_random_integers():
@@ -36,5 +38,18 @@ def gen_random_doubles():
     with open(file=f'{FILENAME_RANDOM_DOUBLES_CPP}{END}', mode="w", encoding="UTF-8") as file:
         file.write(cpp_array)
 
-gen_random_integers()
-gen_random_doubles()
+def gen_sorted_integers():
+    arr = [x for x in range(NUM)]
+    cpp_array = "{"
+    with open(file=f'{FILENAME_SORTED_INTEGERS}{END}', mode="w", encoding="UTF-8") as file:
+        for num in arr:
+            cpp_array += f'{str(num)},'
+            file.write(str(num)+"\n")
+
+    cpp_array = cpp_array[:-1] + "}"
+    with open(file=f'{FILENAME_SORTED_INTEGERS_CPP}{END}', mode="w", encoding="UTF-8") as file:
+        file.write(cpp_array)
+
+#gen_random_integers()
+#gen_random_doubles()
+gen_sorted_integers()
